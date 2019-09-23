f7a95641d2016ca469d6ebe286c2ea9b4cefe421
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2019-09-23 23:36:22 +0900

    Add social links

--- src/component/top/SocialLinks.tsx
+++ src/component/top/SocialLinks.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import {Grid, makeStyles} from "@material-ui/core";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles({
-    socialIcon: {
-        padding: "10px 15px",
-        margin: "50px 0px",
+    social: {
+        margin: "50px 0",
     },
+    socialIcon: {
+        margin: "0px 15px",
+    }
 });
 
 const SocialLinks: React.FC = () => {
     const classes = useStyles();
 
     return (
-        <Grid item xs={12} container justify="center" alignItems="center">
-            <FontAwesomeIcon size="2x" icon={['fab', 'facebook']} className={classes.socialIcon} />
-            <FontAwesomeIcon size="2x" icon={['fab', 'github']} className={classes.socialIcon} />
-            <FontAwesomeIcon size="2x" icon={['fab', 'instagram']} className={classes.socialIcon} />
-            <FontAwesomeIcon size="2x" icon={['fab', 'linkedin']} className={classes.socialIcon} />
-            <FontAwesomeIcon size="2x" icon="sticky-note" className={classes.socialIcon} />
+        <Grid item xs={12} container justify="center" alignItems="center" className={classes.social}>
+            <div className={classes.socialIcon}>
+                <a href="https://www.facebook.com/tosimitu.kugimoto"><FontAwesomeIcon size="2x" icon={['fab', 'facebook']} /></a>
+            </div>
+            <div className={classes.socialIcon}>
+                <a href="https://github.com/tkugimot"><FontAwesomeIcon size="2x" icon={['fab', 'github']} /></a>
+            </div>
+            <div className={classes.socialIcon}>
+                <a href="https://www.instagram.com/toshimitsukugimoto"><FontAwesomeIcon size="2x" icon={['fab', 'instagram']} /></a>
+            </div>
+            <div className={classes.socialIcon}>
+                <a href="https://www.linkedin.com/in/toshimitsu-kugimoto/"><FontAwesomeIcon size="2x" icon={['fab', 'linkedin']} /></a>
+            </div>
+            <div className={classes.socialIcon}>
+                <a href="https://note.mu/tkugimot"><FontAwesomeIcon size="2x" icon="sticky-note" /></a>
+            </div>
         </Grid>
     );
 };
